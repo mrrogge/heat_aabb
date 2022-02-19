661990d6ac00e9fc9d46340333aaab2b3cbd9493
mrrogge <5900418+mrrogge@users.noreply.github.com>
2022-02-19 16:11:50 -0600

    tests

--- test/Main.hx
+++ test/Main.hx
@@ -6,8 +6,8 @@ using buddy.Should;
 
 class Main extends buddy.SingleSuite {
     public function new() {
-        describe("test", {
-            it("test", {
+        describe("basic tests:", {
+            it("basic collision", {
                 var coms = new ComMap<Collidable>();
                 var sys = new CollisionSys(coms);
                 coms[1] = new Collidable().setPos(0, 0).setDim(50, 50);
