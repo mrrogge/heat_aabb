bb9e685fa7da7be1c03e9f43c4ce4cdb4fc7ee4a
qumrr1 <Matthew_R_Rogge@graco.com>
2023-01-30 21:36:59 -0600

    add vectors to interface

--- src/heat/aabb/IRect.hx
+++ src/heat/aabb/IRect.hx
@@ -1,14 +1,19 @@
 package heat.aabb;
 
+using heat.AllCore;
+
 interface IRect {
+    public var pos(default, null):IVector2<Float>; 
+    public var x(get, never):Float;
+    public var y(get, never):Float;   
     public var leftX(get, never):Float;
     public var rightX(get, never):Float;
     public var topY(get, never):Float;
     public var bottomY(get, never):Float;
+    public var dim(default, null):IVector2<Float>;
     public var width(get, never):Float;
     public var height(get, never):Float;
+    public var offset(default, null):IVector2<Float>;
     public var offsetX(get, never):Float;
     public var offsetY(get, never):Float;
-    public var x(get, never):Float;
-    public var y(get, never):Float;    
 }
